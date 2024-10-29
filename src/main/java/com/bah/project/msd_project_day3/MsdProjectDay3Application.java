8b90eb26964b2112de41a10964928a383582e691
sp1212 <stuart.paine12@gmail.com>
2024-10-29 15:20:54 +0000

    Initial setup

---
+++ src/main/java/com/bah/project/msd_project_day3/MsdProjectDay3Application.java
@@ -0,0 +1,13 @@
+package com.bah.project.msd_project_day3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsdProjectDay3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsdProjectDay3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bah/project/msd_project_day3/MsdProjectDay3ApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.bah.project.msd_project_day3;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MsdProjectDay3ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
